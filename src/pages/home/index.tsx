7a8bbb7a8384da255ec0d7245e9a715a442e2aa8
Gunwoo Kim <gunoo0126@gmail.com>
2023-06-28 23:49:58 +0900

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const HomesPage: NextPage = () => {
+  return (
+    <div>
+      <h1>This is the home Page</h1>
+      <p>Welcome to the home Page!</p>
+    </div>
+  );
+};
+
+export default HomesPage;
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const TestPage: NextPage = () => {
+  return (
+    <div>
+      <h1>This is the Test Page</h1>
+      <p>Welcome to the Test Page!</p>
+    </div>
+  );
+};
+
+export default TestPage;
